14c37824b16d31bc13ef12963a916c07770e7707
Abdullah <abdullah.ali26@hotmail.com>
2020-05-14 02:30:41 +0200

    feat: installing react-navigation !!

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import { Provider } from "react-redux";
 import reducer from "./Reducers/";
 import middleware from "./Middlewares";
 import Home from "./Components/Home";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 export default function App() {
   const store = createStore(reducer, middleware);
   return (
