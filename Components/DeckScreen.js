fd96a149a627f296f9ed596649883dd941c9f0bb
Abdullah <abdullah.ali26@hotmail.com>
2020-05-15 21:42:47 +0200

    feat: styling deck screen

--- Components/DeckScreen.js
+++ Components/DeckScreen.js
@@ -1,12 +1,20 @@
 import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
 import { connect } from "react-redux";
-import DeckHeader from "./Home/DeckHeader";
 import { handleDeleteDeck } from "../Actions/decks";
 import { CommonActions } from "@react-navigation/native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
 function DeckScreen(props) {
   console.log("Deck screen props", props);
   const { deckId } = props.route.params;
+  props.navigation.setOptions({ title: props.deckTitle });
   const onDeleteDeck = () => {
     props.dispatch(handleDeleteDeck(deckId));
     props.navigation.dispatch(CommonActions.goBack());
@@ -19,26 +27,62 @@ function DeckScreen(props) {
   };
   return (
     <View style={styles.container}>
-      <DeckHeader deckId={deckId} />
-      <TouchableOpacity onPress={onDeleteDeck}>
-        <Text>Delete Deck</Text>
+      <Text style={{ fontSize: 18, marginBottom: 30 }}>
+        {props.cardsCount} Cards
+      </Text>
+      <TouchableOpacity
+        style={[styles.btn, styles.row]}
+        onPress={onAddCardPress}
+      >
+        <MaterialCommunityIcons name="library-plus" color="white" size={15} />
+        <Text style={{ color: "white" }}> Add Card</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={onAddCardPress}>
-        <Text>Add Card</Text>
+      <TouchableOpacity style={[styles.btn, styles.row]} onPress={onStartQuiz}>
+        <MaterialCommunityIcons
+          name="comment-question"
+          color="white"
+          size={15}
+        />
+        <Text style={{ color: "white" }}> Start Quiz</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={onStartQuiz}>
-        <Text>Start Quiz</Text>
+      <TouchableOpacity
+        style={[styles.btn, styles.delete, styles.row]}
+        onPress={onDeleteDeck}
+      >
+        <MaterialCommunityIcons name="delete" color="white" size={15} />
+        <Text style={{ color: "white" }}> Delete Deck</Text>
       </TouchableOpacity>
     </View>
   );
 }
-export default connect()(DeckScreen);
+export default connect(({ decks }, props) => ({
+  deckTitle: decks[props.route.params.deckId].title,
+  cardsCount: decks[props.route.params.deckId].questions.length,
+}))(DeckScreen);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
+    paddingTop: 20,
+  },
+  btn: {
+    borderWidth: 1,
+    width: Dimensions.get("window").width * 0.75,
+    borderRadius: 5,
+    borderColor: "#ccc",
+    height: 38,
     justifyContent: "center",
+    backgroundColor: "#00b2ff",
+    margin: 5,
+    alignItems: "center",
+  },
+  delete: {
+    marginTop: 40,
+    backgroundColor: "#ff00b2",
+  },
+  row: {
+    flexDirection: "row",
   },
 });
